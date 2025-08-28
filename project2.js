4ed809b9d0113daca4db996e03cbb0da53b516e4
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-08-28 13:51:00 +0530

    change in project 2

--- project2.js
+++ project2.js
@@ -6,10 +6,10 @@ const weight =  parseInt(document.querySelector('#weight').value)
 const result = document.querySelector('#result')
 
 if(hight === '' || isNaN(hight) || hight <= 0){
-    result.innerHTML = 'Please provide a valid height'
+    results.innerHTML = 'Please provide a valid height'
 }
-else if (hight === '' || isNaN(weight) || hight <= 0){
-    result.innerHTML = 'Please provide a valid height'
+else if (weight === '' || isNaN(weight) || weight <= 0){
+    results.innerHTML = 'Please provide a valid height'
 }
 else {
     const bmi = (weight / ((hight * hight)/10000)).toFixed(2)

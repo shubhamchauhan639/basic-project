b56302554885e33717c91e79f21573cd770a1c08
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-09-10 21:39:48 +0530

    project 4

---
+++ project4.js
@@ -0,0 +1,24 @@
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = "#"
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor( Math.random()*16)]
+    }
+    return color
+}
+let intervalId 
+let startChangingColor = function(){
+    if (!intervalId) {
+ intervalId =  setInterval(changeBgcolor,1000)
+    }
+ 
+   function changeBgcolor(){
+    document.body.style.backgroundColor = randomColor();
+   } 
+}
+let stopChangingColor = function(){
+clearInterval(intervalId)
+intervalId = null
+}
+document.querySelector("#start").addEventListener("click",startChangingColor)
+document.querySelector("#stop").addEventListener("click",stopChangingColor)
